5da689e2e3d9bbed32a47d32443c2ac59237ff62
Olga Nadongo <winnienadongo@gmail.com>
2022-07-24 13:50:29 +0300

    parent-child communication

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,15 @@ function App() {
     setIsDisplay(!isDisplay);
   }
 
+  function addPoem(newPoem){
+      console.log(newPoem)
+  }
 
   return (
     <div className="app">
       <div className="sidebar">
         <button onClick={handleClick}>Show/hide new poem form</button>
-        {isDisplay ? <NewPoemForm /> : null}
+        {isDisplay ? <NewPoemForm  addPoem={addPoem}/> : null}
       </div>
       <PoemsContainer />
     </div>
--- src/components/NewPoemForm.js
+++ src/components/NewPoemForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function NewPoemForm() {
+function NewPoemForm({addPoem}) {
 
    const [poemData, setPoemData]= useState({
     title:"",
@@ -10,16 +10,43 @@ function NewPoemForm() {
   
   function handleChange(event){
     setPoemData({...poemData,[event.target.name]:event.target.value})
-    console.log(poemData)
+    //console.log(poemData)
   }
 
   // function handleTitleChange(event){
   //   setPoemData({...poemData,title:event.target.value})
   // }
+  function handleSubmitForm(event)
+  {
+     event.preventDefault()  
 
+      fetch("http://localhost:8004/poems",
+      {method:"POST",
+      headers:
+      {"Content-Type":"Application/json",
+      "Accept":"Application/json"},
+      body:JSON.stringify(poemData)
+       })
+      .then(r=>r.json())
+
+      .then(data=>{
+
+        //console.log(data)
+        setPoemData( {
+          title:"",
+          content: "",
+          author: ""
+        })
+
+        addPoem(data)
+      })
+      .catch(error=>console.log(error))
+    
+  }
+    
 
   return (
-    <form className="new-poem-form">
+    <form className="new-poem-form" onSubmit={handleSubmitForm}>
       <input placeholder="Title" name="title" value={poemData.title} onChange={handleChange}/>
       <input placeholder="Author" name="author"  value={poemData.author}  onChange={handleChange}/>
       <textarea placeholder="Write your masterpiece here..." rows={10} name="content" value={poemData.content} onChange={handleChange}/>
