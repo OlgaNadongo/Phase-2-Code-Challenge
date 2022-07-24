a23b22290fc42fc2d85ebf7f0958f80ad1939745
Olga Nadongo <winnienadongo@gmail.com>
2022-07-24 21:22:30 +0300

    updated favorite button and filter

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
 
-function Poem({title ,content ,author}) {
+function Poem({id,title ,content ,author, fav}) {
   const[isRead, setIsRead]=useState(false)
+  const[isFav, setIsFav]=useState(fav)
 
   function handleClick(){
     setIsRead(!isRead)
   }
+
+  function changeFav(){
+
+    fetch(`http://localhost:8004/poems/${id}`,{
+      method: "PATCH",
+      headers:{
+        "Content-Type":"Application/json",
+        "Accept":"Application/json"
+      },
+      body: JSON.stringify({isFav:!isFav})
+    }
+    )
+    
+    .then(r=>r.json())
+    .then(data=>setIsFav(!isFav))
+    .catch(error=>console.log(error))
+  }
+
+
+  
+  
+
+
+
   return (
     <div>
       <h3>{title}</h3>
@@ -14,6 +39,7 @@ function Poem({title ,content ,author}) {
         <strong>- By {author}</strong>
       </p>
       <button onClick={handleClick}>{isRead? "Mark as unread":"Mark as read"}</button>
+      <button onClick={changeFav}>{isFav? "Remove from Fav":"Add to Fav" }</button>
     </div>
   );
 }
--- src/components/PoemsContainer.js
+++ src/components/PoemsContainer.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
 import Poem from "./Poem";
 
 function PoemsContainer({poems}) {
+
+ const[selectCategory, setSelectCategory]=useState("All")
+
+  function handleCategory(event){
+    setSelectCategory(event.target.value)
+  }
+
+  
+
+  const filteredPoems=poems.filter(poem=>{
+    if(selectCategory==="All") return true;
+
+    return poem.isFav=== true && selectCategory ==="favorite"
+  })
+
  
   return (
     <div className="poems-container">
-      {/* render a list of <Poem> components in here */}
+       <select onChange={handleCategory}  value={selectCategory}>
+          
+          <option value="All">All Poems</option>
+          <option value="favorite">Favorites</option>
+       </select>
       {
-        poems.map(poem=><Poem key={poem.id} title={poem.title} content={poem.content} author={poem.author} />
+        filteredPoems.map(poem=><Poem key={poem.id} id={poem.id} title={poem.title} content={poem.content} author={poem.author} fav={poem.isFav}/>
         )
       }
     </div>
